d89067a55311312422098abd4b9e355219c1c398
nitesh-jain-avalara <nitesh.jain@avalara.com>
2016-04-11 11:06:37 +0530

    new run file chnage

--- run.bat
+++ run.bat
@@ -1,2 +1,15 @@
 ::set classpath=C:\Users\nitesh.jain\workspace\WooCommerce1\bin;C:\Users\nitesh.jain\workspace\WooCommerce1\lib\*;
 ::java -cp bin;lib/* org.testng.TestNG testng.xml
+@echo off
+::Ask
+::echo Your Source Path:
+::set INPUT1=
+::set /P INPUT1=Type input: %=%
+::echo Your Destination Path:
+::set INPUT2=
+::set /P INPUT2=Type input: %=%
+::xcopy %INPUT1% %INPUT2% /y /s
+
+set INPUT1=C:\Users\nitesh.jain\Desktop\screen
+set INPUT2=C:\Users\nitesh.jain\Desktop\motog
+xcopy %INPUT1% %INPUT2% /y /s
